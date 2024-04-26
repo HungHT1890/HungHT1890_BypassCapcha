3ee93bb96a9737dc0c50b4483910139ffc765342
HungHT1890 <hungsaki2003@gmail.com>
2024-04-26 10:16:51 +0700

    up

--- Bypass_Recaptcha_WhisperWeb/main.py
+++ Bypass_Recaptcha_WhisperWeb/main.py
@@ -2,9 +2,15 @@ from requests import session
 ss = session()
 
 def convert_speech_text(file_path,api_key):
-    file = {'file': open(file_path,'rb')}
-    res = ss.post(url='https://transcribe.whisperapi.com',headers={'Authorization': f'Bearer {api_key}'},files=file).json()['text']
-    return res
+    results = ''
+    try:
+        file = {'file': open(file_path,'rb')}
+        res = ss.post(url='https://transcribe.whisperapi.com',headers={'Authorization': f'Bearer {api_key}'},files=file).json()
+        results = res['text']
+    except:
+        pass
+    return results
+    
 
 
 
@@ -12,5 +18,5 @@ def convert_speech_text(file_path,api_key):
 if __name__ == '__main__':
     api_key = 'LFHLTUXWNDLT1ENZNKLS5QW53TLN8XLN'
     file_path = 'audio.mp3'
-    # results = convert_speech_text(file_path,api_key)
-    # print(results)
+    results = convert_speech_text(file_path,api_key)
+    print(results)
--- Bypass_Recaptcha_WhisperWeb/test.py
+++ Bypass_Recaptcha_WhisperWeb/test.py
@@ -5,11 +5,12 @@ from selenium.webdriver.common.by import By
 from requests import session
 from os.path import abspath
 _service = ChromeService('chromedriver.exe')
+
+
 def setup_driver():
     options = ChromeOptions()
     options.add_argument('--proxy-server=127.0.0.1:40000')
     return Chrome(service=_service,options=options)
-
 
 def get_audio_captcha(driver,file_name='test.mp3'):
     driver.get('https://www.google.com/recaptcha/api2/demo')
@@ -19,7 +20,7 @@ def get_audio_captcha(driver,file_name='test.mp3'):
     WebDriverWait(driver,10).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH,'//*[contains(@title,"expires")]')))
     WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,'//*[@id="recaptcha-audio-button"]'))).click()
     audio_url = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,'//*[@class="rc-audiochallenge-tdownload-link"]'))).get_attribute('href')
-    audio_name = 'test.mp3'
+    audio_name = file_name
     with open(audio_name,'wb') as file_audio:
         file_audio.write(session().get(audio_url).content)
 
@@ -30,12 +31,11 @@ def send_audio(driver,file_path='test.mp3'):
     source = driver.page_source
     results = source.split('<h3>Transcription Result:</h3><p>')[1].split('</p>')[0]
     print(results)
-    with open('test.txt','w',encoding='utf-8') as file_source:
-        file_source.write(str(driver.page_source))
+
         
 if __name__ == '__main__':
     driver = setup_driver()
-    # get_audio_captcha(driver)
+    get_audio_captcha(driver)
     send_audio(driver)
     input("Enter to close")
     driver.close()    
